f45d13e1a1288195e213849ed0ec0a085c95b013
JFP3 <jfpardini@gmail.com>
2014-12-15 15:54:52 +0100

    Email.

--- web.rb
+++ web.rb
@@ -10,23 +10,4 @@ class Web < Sinatra::Base
   get "/" do
     redirect '/index.html'
   end
-
-  post '/shop.html' do
-    Pony.mail({
-    :from => params[:name],
-    :to => 'ubersapiens@gmail.com',
-    :subject => params[:name] + " " + "has contacted you via the Pardini Coffee Co.",
-    :body => params[:mail],
-    :via => :smtp,
-    :via_options => {
-     :address              => 'smtp.gmail.com',
-     :port                 => '587',
-     :enable_starttls_auto => true,
-     :user_name            => 'ubersapiens@gmail.com',
-     :password             => 'x',
-     :authentication       => :plain, 
-     :domain               => "pardinicoffee.herokuapp.com" 
-     } 
-    })  
-  end
 end
