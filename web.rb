bfa94763b40dc51c098a4fe25e9d959bc94fd342
JFP3 <jfpardini@gmail.com>
2014-12-15 13:54:42 +0100

    Contact form.

--- web.rb
+++ web.rb
@@ -1,16 +1,32 @@
 require 'sinatra/base'
+require 'pony'
 
 class Web < Sinatra::Base
 
-configure do
+  configure do
+    set :public_folder, 'public'
+  end
 
-  set :public_folder, 'public'
+  get "/" do
+    redirect '/index.html'
+  end
 
-end
-
-# route to starting page
-get "/" do
-  redirect '/index.html'
-end
-
+  post '/shop.html' do
+    Pony.mail({
+    :from => params[:name],
+    :to => 'ubersapiens@gmail.com',
+    :subject => params[:name] + " " + "has contacted you via the Pardini Coffee Co.",
+    :body => params[:mail],
+    :via => :smtp,
+    :via_options => {
+     :address              => 'smtp.gmail.com',
+     :port                 => '587',
+     :enable_starttls_auto => true,
+     :user_name            => 'ubersapiens@gmail.com',
+     :password             => 'Jfp160885',
+     :authentication       => :plain, 
+     :domain               => "pardinicoffee.herokuapp.com" 
+     } 
+    })  
+  end
 end
